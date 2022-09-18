bcb5579c7fcad98d0a8e1bfd65fd49dfc9c1f568
eVery1337 <omegastrikeclan@yandex.ru>
2022-09-18 13:19:32 +0300

    Initial commit

---
+++ generator/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+def password(request):
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    upperCharacters = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    symbols = list('!"#$%&')
+    length = 10
+    password = ''
+
+    for x in range(length):
+        password+=random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password':password})
